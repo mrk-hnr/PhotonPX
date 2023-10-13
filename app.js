929b6fbebb2310255c2003f35dd3bc5166e6d583
mrk-hnr <mrk.hnr@hotmail.com>
2023-10-13 14:41:01 +0800

    Refactored API URLs to be able to create dynamic More button wherein More images may be curated or not

--- app.js
+++ app.js
@@ -2,18 +2,25 @@ const auth = "" // Your PEXELS AUTH HERE
 const gallery = document.querySelector(".gallery")
 const searchInput = document.querySelector(".search-input")
 const form = document.querySelector(".search-form")
+const more = document.querySelector(".more")
+
 
 let searchValue
-
+let page = 1
+let fetchLink
+let currentSearch
 
 // EVENT LISTENER
 searchInput.addEventListener("input", updateInput)
 form.addEventListener("submit", (event) => {
     event.preventDefault()
+    currentSearch = searchValue
     searchPhotos(searchValue)
 })
 
+more.addEventListener("click", loadMore)
 
+// FUNCTIONS
 function updateInput(event) {
     searchValue = event.target.value
 }
@@ -34,6 +41,7 @@ function generatePictures(data) {
     data.photos.forEach(photo => { 
         const galleryImg = document.createElement("div")
         galleryImg.classList.add("gallery-image")
+
         galleryImg.innerHTML = `
         <div class="gallery-info>
         <p>${photo.photographer}</p>
@@ -45,7 +53,8 @@ function generatePictures(data) {
 }
 
 async function curatedPhotos() {
-    const data = await fetchAPI(`https://api.pexels.com/v1/curated?per_page=15&page=1`)
+    fetchLink = `https://api.pexels.com/v1/curated?per_page=15&page=1`
+    const data = await fetchAPI(fetchLink)
     generatePictures(data)
 }
 
@@ -53,13 +62,27 @@ async function curatedPhotos() {
 // Syntax of searchPhotos() is exact copy of curatedPhotos() except the fetch URL
 async function searchPhotos(query) {
     clear()
-    const data = fetchAPI(`https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`)
+    fetchLink = `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`
+    const data = fetchAPI(fetchLink)
     generatePictures(data)
 }
 
 function clear() {
     gallery.innerHTML = ""
     searchInput.value = ""
+}
+
+async function loadMore() {
+    page++
+
+    // Checks if page displayed is curated or not
+    if (currentSearch) {
+        fetchLink = `https://api.pexels.com/v1/search?query=${currentSearch}+query&per_page=15&page=${page}`
+    } else {
+        fetchLink = `https://api.pexels.com/v1/curated?per_page=15&page=${page}`
+    }
+    const data = await fetchAPI(fetchLink)
+    generatePictures(data)
 }
 
 curatedPhotos()
